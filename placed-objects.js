3c304877dcc9c0256617165c4e657882bc1c721d
midirectiekade14-debug <midirectiekade14@gmail.com>
2026-04-03 18:14:03 +0200

    Plattegrond bijgewerkt via admin (3-4-2026, 18:13:59)

--- placed-objects.js
+++ placed-objects.js
@@ -1,5 +1,5 @@
 // Werkplaats17 -- Geplaatste objecten
-// Laatst gewijzigd: 2026-04-03T16:05:13.395Z
+// Laatst gewijzigd: 2026-04-03T16:13:59.775Z
 // Gegenereerd via admin.html
 
 const PLACED_OBJECTS = [];
