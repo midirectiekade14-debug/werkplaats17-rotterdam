61dbe2a7d9f45e2a35ce3bd9f046455b1e0f2523
midirectiekade14-debug <midirectiekade14@gmail.com>
2026-04-03 22:04:33 +0200

    Plattegrond bijgewerkt via admin (3-4-2026, 22:04:31)

--- placed-objects.js
+++ placed-objects.js
@@ -1,5 +1,90 @@
 // Werkplaats17 -- Geplaatste objecten
-// Laatst gewijzigd: 2026-04-03T16:55:47.180Z
+// Laatst gewijzigd: 2026-04-03T20:04:31.752Z
 // Gegenereerd via admin.html
 
-const PLACED_OBJECTS = [];
+const PLACED_OBJECTS = [
+  {
+    "floor": 0,
+    "x": 549,
+    "y": 738,
+    "w": 900,
+    "h": 450,
+    "l": "Magazijn",
+    "c": "#e09a2f",
+    "bg": "rgba(224,154,47,0.13)",
+    "type": "",
+    "rot": "0",
+    "status": "",
+    "room": "1"
+  },
+  {
+    "floor": 0,
+    "x": 639,
+    "y": 738,
+    "w": 153,
+    "h": 144,
+    "l": "Kantoor",
+    "c": "rgba(110,150,220,.5)",
+    "bg": "rgba(110,150,220,.13)",
+    "type": "",
+    "rot": "0",
+    "status": "",
+    "room": "1"
+  },
+  {
+    "floor": 0,
+    "x": 1290,
+    "y": 1149,
+    "w": 45,
+    "h": 24,
+    "l": "Trap",
+    "c": "rgba(180,140,80,.45)",
+    "bg": "rgba(180,140,80,.13)",
+    "type": "",
+    "rot": "0",
+    "status": "",
+    "room": "1"
+  },
+  {
+    "floor": 0,
+    "x": 1404,
+    "y": 1125,
+    "w": 45,
+    "h": 60,
+    "l": "Entree",
+    "c": "rgba(100,180,220,.5)",
+    "bg": "rgba(100,180,220,.13)",
+    "type": "",
+    "rot": "0",
+    "status": "",
+    "room": "1"
+  },
+  {
+    "floor": 0,
+    "x": 1359,
+    "y": 1125,
+    "w": 45,
+    "h": 45,
+    "l": "Toilet",
+    "c": "rgba(120,160,220,.5)",
+    "bg": "rgba(120,160,220,.18)",
+    "type": "",
+    "rot": "0",
+    "status": "",
+    "room": "1"
+  },
+  {
+    "floor": 0,
+    "x": 552,
+    "y": 741,
+    "w": 87,
+    "h": 135,
+    "l": "Berging",
+    "c": "rgba(160,140,120,.5)",
+    "bg": "rgba(160,140,120,.13)",
+    "type": "",
+    "rot": "0",
+    "status": "",
+    "room": "1"
+  }
+];
